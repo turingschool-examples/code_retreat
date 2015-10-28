7bef728e5e7d3d3d6ea02edfc4f060a86c84ffd2
Jeff Casimir <jeff@casimircreative.com>
2015-10-28 10:57:26 -0600

    Too Much Battleship

--- battleship/test/battleship_test.rb
+++ battleship/test/battleship_test.rb
@@ -3,11 +3,5 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class BattleshipTest < Minitest::Test
-  def assert_alive(boat)
-    assert boat.alive?
-  end
-
-  def assert_dead(boat)
-    refute boat.alive?, "expected #{boat.inspect} to be sunk"
-  end  
+  
 end
